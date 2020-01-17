295077eafc9e15f64d15e621fb4f745f93b43c40
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-01-17 17:13:21 -0300

    task/checkin-24hs

--- src/components/Detail/Accommodation/CheckInOut/CheckInOut.tsx
+++ src/components/Detail/Accommodation/CheckInOut/CheckInOut.tsx
@@ -4,7 +4,6 @@ import Description from '../../../Description/Description';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Skeleton from 'react-loading-skeleton';
 
-
 export interface CheckInOutLoadingProps {
   checkInOut: CheckInOutProps;
   loading: boolean;
@@ -42,15 +41,26 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const CheckInCheckOut: FunctionComponent<CheckInOutLoadingProps> = props => {
+  const begin24Hours = "0:00";
+  const end24Hours = "23:59";
 
   const classes = useStyles();
 
-  const checkInEndTime =props.checkInOut.checkIn.endTime
-    && props.checkInOut.checkIn.endTime !== props.checkInOut.checkIn.beginTime  
-    ? `a ${props.checkInOut.checkIn.endTime} Hs.` : "";
-
-  const checkInHour = () => <div> {props.checkInOut.checkIn.beginTime ? `A partir de ${props.checkInOut.checkIn.beginTime} Hs.` : ""} {checkInEndTime}</div>;
-  const checkOutHour = () => <div> {props.checkInOut.checkOut.time ? `Hasta las ${props.checkInOut.checkOut.time} Hs.` : ""}</div>;
+  const checkInHour = () => {
+    if(props.checkInOut.checkIn.beginTime == begin24Hours && props.checkInOut.checkIn.endTime == end24Hours) {
+      return <Box> Las 24 Hs.</Box>;  
+    } else if(props.checkInOut.checkIn.beginTime && props.checkInOut.checkIn.endTime && props.checkInOut.checkIn.beginTime != props.checkInOut.checkIn.endTime) {
+      return <Box>A partir de {props.checkInOut.checkIn.beginTime} Hs. a {props.checkInOut.checkIn.endTime} Hs.</Box>;
+    } else if(props.checkInOut.checkIn.beginTime) {
+      return <Box>A partir de {props.checkInOut.checkIn.beginTime} Hs.</Box>
+    } else if(props.checkInOut.checkIn.endTime) {
+      return <Box>Hasta las {props.checkInOut.checkIn.endTime} Hs.</Box>
+    } else {
+      return <Box></Box>;
+    }
+  }
+  
+  const checkOutHour = () => <Box> {props.checkInOut.checkOut.time ? `Hasta las ${props.checkInOut.checkOut.time} Hs.` : ""}</Box>;
 
   return <Grid container item spacing={2} alignItems="flex-start">
     <Grid item xs={12} className="otravo-title">
