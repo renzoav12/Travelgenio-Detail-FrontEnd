da1ab3eccb5425ceb5dd672953924a3be1a54f43
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-03-27 11:39:20 -0300

    Move map to its own section

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,6 +1,7 @@
 import React, {FunctionComponent} from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import Accommodation, { AccommodationProps } from './Accommodation/Accommodation';
+import Map from './Map/Map';
 import Availability from './Availability/Availability';
 import { RoomDetail } from './Availability/Room/Room';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
@@ -20,6 +21,9 @@ const useStyles = makeStyles((theme: Theme) =>
     accommodation: {
       marginTop: 20
     },
+    map: {
+      marginTop: 20
+    },
     availabilities: {
       marginTop: 20,
       marginBottom: 20
@@ -34,6 +38,9 @@ const Detail: FunctionComponent<DetailProps> = props => {
   return <Grid container>
            <Grid item xs={12} className={classes.accommodation}>
             <Accommodation accommodation={props.accommodation} loading={props.accommodationLoading}/>
+          </Grid>
+          <Grid item xs={12} className={classes.map}>
+            <Map accommodation={props.accommodation} loading={props.accommodationLoading}/>
           </Grid>
           <Grid item xs={12} className={classes.availabilities}>
             <Availability rooms={props.rooms} occupancy={props.occupancy} loading={props.roomsLoading} onSelect={props.onSelect}/>
