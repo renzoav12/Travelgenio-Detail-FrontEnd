f3bf13710ddd82fe974641be9cd7bc757696e58c
Renzo <renzo.avila@travelgenio.com>
2020-03-30 10:54:00 -0300

    change value status gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -7,10 +7,11 @@ import Img from 'react-image';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import Image from '../Image/Image';
+import { Status } from '../../model/search';
 
 export interface GalleryProps {
   images: Array<Image>;
-  loading: string;
+  loading: Status;
 }
 
 export interface Image {
@@ -158,7 +159,7 @@ const Gallery: FunctionComponent<GalleryProps> = props => {
     />;
   
   return <Box className={classes.gallery}>
-      {(props.loading === 'loading') ? loadingImage : gallery}
+      {(props.loading === Status.LOADING) ? loadingImage : gallery}
     </Box>;
 }
 
