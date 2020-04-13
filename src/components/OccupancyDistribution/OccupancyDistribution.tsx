cdadb965f3098d9677e6a101ea1e675f8fa467ec
Renzo <renzo.avila@travelgenio.com>
2020-04-13 11:52:40 -0300

    HTL-595 change occupancy distribution for used component

--- src/components/OccupancyDistribution/OccupancyDistribution.tsx
+++ src/components/OccupancyDistribution/OccupancyDistribution.tsx
@@ -1,23 +1,23 @@
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
 import { SearchBoxOccupancyState } from '@hotels/search-box';
 import { Grid, Box } from '@material-ui/core';
-import { Person }  from '@material-ui/icons';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { BedGroup } from '../Detail/Availability/Room/Room';
+import Occupancy from '@hotels/occupancy';
 
-export const serializeOccupancy = (occupancy: SearchBoxOccupancyState) : string => {
-  return occupancy.rooms.map(room => room.adults + ((room.childrenAges.length === 0) ? "" : "-") +room.childrenAges.join("-")).join("!");
+
+export const serializeOccupancy = (occupancy: SearchBoxOccupancyState): string => {
+  return occupancy.rooms.map(room => room.adults + ((room.childrenAges.length === 0) ? "" : "-") + room.childrenAges.join("-")).join("!");
 }
 
 export const parseOccupancy = (searchOccupancy: string): SearchBoxOccupancyState => {
   let rooms = searchOccupancy.split("!")
-      .map(room => {
-        let guests = room.split("-"); 
-        return {
-          adults: parseInt(guests[0]),
-          childrenAges: guests.slice(1,guests.length).map(age => parseInt(age))
-        };
-      });
+    .map(room => {
+      let guests = room.split("-");
+      return {
+        adults: parseInt(guests[0]),
+        childrenAges: guests.slice(1, guests.length).map(age => parseInt(age))
+      };
+    });
   return { rooms };
 }
 
@@ -25,41 +25,18 @@ export interface OccupancyProps {
   occupancy: SearchBoxOccupancyState;
   bedGroup: BedGroup;
 }
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    room: {
-      verticalAlign: "middle",
-      display: "flex"
-    },
-    roomText: {
-      paddingTop: 4
-    },
-    people: {
-      marginLeft: 5
-    }
-  })
-);
+ 
 
 const OccupancyDistribution: FunctionComponent<OccupancyProps> = props => {
-  
-  const classes = useStyles();
-
-  const rooms: Array<number> = props.occupancy.rooms.map(room => room.adults + room.childrenAges.length);
-  
-  const guests = (quantity: number): JSX.Element => {
-    let people: Array<JSX.Element> = [];
-
-    for(let index = 0; index < quantity; index++) {
-      people.push(<Person key={index}/>)
-    }
-
-  return <Box className={classes.room}><Box className={classes.roomText}>1 habitación con {props.bedGroup.description} para</Box> <Box className={classes.people}>{people}</Box></Box>;
-  }
 
   return <Grid container>
-    {rooms.map((room, index) => <Grid item xs={12} key={index}>{guests(room)}</Grid>)}
-  </Grid>;
+    {props.occupancy.rooms.map((room,index) => 
+    <Grid item xs={12} key={index}> 
+       <Occupancy  adults={room.adults}
+        childrenAges={room.childrenAges}
+        description={props.bedGroup.description}>
+      </Occupancy>
+    </Grid>)} </Grid>;
 }
 
 export default OccupancyDistribution;
