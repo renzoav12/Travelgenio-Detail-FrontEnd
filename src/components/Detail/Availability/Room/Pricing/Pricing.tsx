ffad7ed99adc0c3db1ef602727f862712e8f494e
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-04-21 12:30:05 -0300

    Add taxes, charges and fees.

--- src/components/Detail/Availability/Room/Pricing/Pricing.tsx
+++ src/components/Detail/Availability/Room/Pricing/Pricing.tsx
@@ -6,6 +6,7 @@ import OccupancyDistribution from '../../../../OccupancyDistribution/OccupancyDi
 import { SearchBoxOccupancyState } from '@hotels/search-box';
 import { BedGroup } from '../Room';
 import  MealPlan, { MealPlanProps } from '@hotels/mealplan';
+import Charges from './Charges/Charges';
 
 export interface PricingProps {
   rate: Rate;
@@ -27,7 +28,15 @@ export interface Rate {
 export interface Breakdown {
   tax: Price;
   payWithoutTax: Price;
+  charges: Array<BreakdownCharge>
 }
+
+export interface BreakdownCharge {
+  description: string;
+  type: string;
+  price: Price;
+}
+
 export interface Price {
   amount: string;
   currency: string;
@@ -53,7 +62,7 @@ const useStyles = makeStyles((theme: Theme) =>
       fontWeight: "bold",
       paddingRight: 20
     },
-    priceDescription: {
+    charges: {
       paddingRight: 20,
       textAlign: "right"
     }
@@ -80,8 +89,10 @@ const Pricing: FunctionComponent<PricingProps> = props => {
     </Grid>
     <Grid container item xs={12} sm={8} md={4} justify="flex-end" alignContent="flex-start">
       <Grid container item xs={12} justify="flex-end" className={classes.price}>{props.rate.stayPrice.amount} {props.rate.stayPrice.currency}</Grid>
-      <Grid container item xs={12} justify="flex-end" className={classes.priceDescription}>{props.rate.nights} noches {props.rate.breakdown.payWithoutTax.amount} {props.rate.breakdown.payWithoutTax.currency}<br/> Impuestos {props.rate.breakdown.tax.amount} {props.rate.breakdown.tax.currency}</Grid>
-      <Grid container item xs={12} justify="flex-end" className={classes.priceDescription}><ExtraCharges {...props.rate.extraCharges}/></Grid>
+      <Grid container item xs={12} justify="flex-end" className={classes.charges}>
+        <Charges {...props}/>
+        <ExtraCharges {...props.rate.extraCharges}/>
+      </Grid>
     </Grid>
     <Grid item xs={12} sm={4} md={2}>
       <Button variant="contained" color="primary" fullWidth onClick={() => props.onSelect(props.rate.id)}>Reservar</Button>
