7009898a52804882401d265827ad339458ea8dfe
Renzo <renzo.avila@travelgenio.com>
2020-05-06 17:24:58 -0300

    HTL613 fix review

--- src/translations.js
+++ src/translations.js
@@ -6,17 +6,17 @@ export default {
         room_pl: "Habitaciones",
         room: "Habitacion",
         search_change_destination: "Modifica tu destino",
-        accommodation_not_checkin: "El alojamiento no informa sobre hora de checkin.",
-        accommodation_not_checkout: "El alojamiento no informa sobre hora de checkout.",
+        accommodation_not_check_in: "El alojamiento no informa sobre hora de entrada.",
+        accommodation_not_check_out: "El alojamiento no informa sobre hora de salida.",
         accommodation_information: "Información",
         accommodation_check_in_out_conditions:"Condiciones del alojamiento",
         accommodation_check_in_from_to: "A partir de {n} Hs. a {m} Hs.",
         accommodation_check_in_from: "A partir de {n} Hs.",
         accommodation_check_in_until: "Hasta las {n} Hs.",
-        accommodation_checkin_24hs: "Las 24 Hs.",
+        accommodation_check_in_24hs: "Las 24 Hs.",
         accommodation_amenities_more_popular: "Servicios más populares",
         accommodation_amenities_services: "Servicios",
-        accommodation_checkin: "Entrada",
-        accommodation_checkout: "Salida"
+        accommodation_check_in: "Entrada",
+        accommodation_check_out: "Salida"
     },
   };
