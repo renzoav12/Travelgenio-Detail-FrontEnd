58286f7fe3c16693fc0975ebe6d59659d77e41fa
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-05-14 14:35:58 -0300

    Google Maps Key config

--- src/config.js
+++ src/config.js
@@ -2,14 +2,17 @@ const dev = {
     ACCOMMODATION_API: "https://dev-hotels-shopping-detail.travelagency.tech",
     ROOMS_API: "https://dev-hotels-shopping-detail.travelagency.tech",
     SUGGESTIONS_API: "https://dev-hotels-autocomplete-suggestion.travelagency.tech",
-    I18N_API: "https://dev-hotels-translation.travelagency.tech"
+    I18N_API: "https://dev-hotels-translation.travelagency.tech",
+    GOOGLE_MAP_KEY: "AIzaSyAnwYNB5CVKjwPORNGDi67XLfcfE0WV-to"
+
 };
 
 const prod = {
     ACCOMMODATION_API: "https://" + window.location.hostname + "/hotels/api/",
     ROOMS_API: "https://" + window.location.hostname + "/hotels/api/",
     SUGGESTIONS_API: "https://" + window.location.hostname + "/hotels/api/",
-    I18N_API: "https://dev-hotels-translation.travelagency.tech"
+    I18N_API: "https://dev-hotels-translation.travelagency.tech",
+    GOOGLE_MAP_KEY: "AIzaSyBNbxebn0wds-Anmg_bPsym7YZcVJqPIK0"
 };
 
 const config = process.env.REACT_APP_STAGE === 'prod'
