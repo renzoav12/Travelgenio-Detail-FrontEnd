30c4829ee5efb98065a033df3b59149f55ded590
Renzo <renzo.avila@travelgenio.com>
2020-05-18 10:59:14 -0300

    change url & variable

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@ const dev = {
     ACCOMMODATION_API: "https://dev-hotels-catalog-accommodation.travelagency.tech",
     ROOMS_API: "https://dev-hotels-shopping-detail.travelagency.tech",
     SUGGESTIONS_API: "https://dev-hotels-autocomplete-suggestion.travelagency.tech",
-    I18N_API: "https://dev-hotels-translation.travelagency.tech"
+    TRANSLATION_API: "https://dev-hotels-translation.travelagency.tech"
 };
 
 const prod = {
     ACCOMMODATION_API: "https://" + window.location.hostname + "/hotels/api/",
     ROOMS_API: "https://" + window.location.hostname + "/hotels/api/",
     SUGGESTIONS_API: "https://" + window.location.hostname + "/hotels/api/",
-    I18N_API: "https://" + window.location.hostname + "/hotels/api/"
+    TRANSLATION_API: "https://" + window.location.hostname + "/hotels/api/"
 };
 
 const config = process.env.REACT_APP_STAGE === 'prod'
