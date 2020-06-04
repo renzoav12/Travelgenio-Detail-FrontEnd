032abc1b366299615efc6992126953e623106ae2
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-06-04 16:15:41 -0300

    add ot-origin-host header

--- src/api/suggestions/suggestions.tsx
+++ src/api/suggestions/suggestions.tsx
@@ -1,6 +1,9 @@
-import axios from 'axios';
-import config from '../../config';
+import axios from "axios";
+import config from "../../config";
 
 export default axios.create({
-    baseURL: config.AUTOCOMPLETE_API
+  baseURL: config.AUTOCOMPLETE_API,
+  headers: {
+    "OT-Origin-Host": window.location.hostname,
+  },
 });
