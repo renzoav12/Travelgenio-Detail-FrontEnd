6fba0e7e6907713fb76cec40746b515cf6d838b8
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-06-30 17:13:16 -0300

    Incomplete path redirect to home

--- src/components/Root/root.routes.constants.ts
+++ src/components/Root/root.routes.constants.ts
@@ -1,5 +1,5 @@
 // tslint:disable:max-line-length
-export const ROUTE_HOME = '/hotels/detail'
+export const ROUTE_DETAIL = '/hotels/detail'
 export const ROUTE_NOT_FOUND = '/hotels/detail/404'
 export const ROUTE_SEARCH_CATALOG = 'hotels/detail/:name-:accommodationId'
 export const ROUTE_OLD_SEARCH_CATALOG = 'hotels/detail/:name-:accommodationId/:language'
--- src/components/Root/root.routes.tsx
+++ src/components/Root/root.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router'
 
 import {
+  ROUTE_DETAIL,
   ROUTE_SEARCH_CATALOG,
   ROUTE_OLD_SEARCH_CATALOG,
   ROUTE_SEARCH_PRICE,
@@ -9,13 +10,17 @@ import {
 } from './root.routes.constants'
 import DetailContainer from '../../containers/Detail'
 
+const homeUrl = window.location.protocol+"//"+window.location.host + "/hotels/home/";
+
 export const getRoutes = () => (
   <div>
     <Switch>
-      <Route path={ROUTE_SEARCH_CATALOG} component={DetailContainer} exact />
-      <Route path={ROUTE_OLD_SEARCH_CATALOG} component={DetailContainer} exact />
-      <Route path={ROUTE_SEARCH_PRICE} component={DetailContainer} exact />
-      <Route path={ROUTE_OLD_SEARCH_PRICE} component={DetailContainer} exact />
+      <Route exact path={ROUTE_SEARCH_CATALOG} component={DetailContainer}/>
+      <Route exact path={ROUTE_OLD_SEARCH_CATALOG} component={DetailContainer} />
+      <Route exact path={ROUTE_SEARCH_PRICE} component={DetailContainer} />
+      <Route exact path={ROUTE_OLD_SEARCH_PRICE} component={DetailContainer} />
+
+      <Route path={ROUTE_DETAIL} render={() => {window.location.href = homeUrl; return null;}}/>
     </Switch>
   </div>
 )
